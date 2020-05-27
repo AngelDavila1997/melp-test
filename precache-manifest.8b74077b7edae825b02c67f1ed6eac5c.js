8e72a5a1117772dd3c2624ae29674f76d10e6a26
Angel Davila <angel.davila19975@gmail.com>
2020-05-27 11:02:25 -0500

    Updates

---
+++ precache-manifest.8b74077b7edae825b02c67f1ed6eac5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74065396ab1a7eaef2324aedf82ab72",
+    "url": "/melp-test/index.html"
+  },
+  {
+    "revision": "0b15c4b2918117ea1d23",
+    "url": "/melp-test/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6bbea4020635f4c6e032",
+    "url": "/melp-test/static/css/main.8020364f.chunk.css"
+  },
+  {
+    "revision": "0b15c4b2918117ea1d23",
+    "url": "/melp-test/static/js/2.ac4e6799.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/melp-test/static/js/2.ac4e6799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbea4020635f4c6e032",
+    "url": "/melp-test/static/js/main.dd5b3578.chunk.js"
+  },
+  {
+    "revision": "c5579bd3798c97bb83b1",
+    "url": "/melp-test/static/js/runtime-main.f47c1bbe.js"
+  }
+]);
